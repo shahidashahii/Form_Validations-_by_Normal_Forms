89442101831ea6dcd23b6740c36ff3999fb99f85
shahidashahii <shahidashahi2000@gmail.com>
2023-05-04 22:40:25 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 def check_for_a(value):
     if value[0]=='a':
@@ -9,4 +10,17 @@ class StudentForm(forms.Form):
     age=forms.IntegerField()
     email=forms.CharField(max_length=100)
     re_enter_email=forms.CharField(max_length=100)
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+    mobile=forms.CharField(max_length=100,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
 
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot')
+        
